1234b71e0c20382b693c84ec2aaadbf1f8cd7485
rwu864 <rwu864@gmail.com>
2016-02-28 21:33:30 -0800

    Created variables that store SP2010 internal names
    It gets annoying having to scroll up and look up the names. This also
    helps with readability.
    
    Format used: sp_[column_name]

--- SPListTest1/Controllers/ListController.cs
+++ SPListTest1/Controllers/ListController.cs
@@ -16,6 +16,21 @@ namespace SPListTest1.Controllers
         string SiteUrl = "http://qtcserver/raymond/";
         string ListName = "Request_Test";
 
+        //Internal Names                ->  List Names 
+        //NewColumn1                    ->  Request ID
+        //Request_x0020_Details         ->  Request Details
+        //Author                        ->  Request By
+        //Request_x0020_Status          ->  Request Status
+        //Request_x0020_Due_x0020_Date  -> Request Due Date
+
+        //these are strings that represent that map the SP2010 list name to the internal names 
+    
+        string sp_Request_ID = "NewColumn1";
+        string sp_Request_Details = "Request_x0020_Details";
+        string sp_Request_By = "Author";
+        string sp_Request_Status = "Request_x0020_Status";
+        string sp_Request_Due_Date = "Request_x0020_Due_x0020_Date";
+
         public ActionResult Items()
         {
             ClientContext clientContext = new ClientContext(SiteUrl);
@@ -27,21 +42,14 @@ namespace SPListTest1.Controllers
             clientContext.Load(spList);
             clientContext.ExecuteQuery();
 
-            //Internal Names                ->  List Names 
-            //NewColumn1                    ->  Request ID
-            //Request_x0020_Details         ->  Request Details
-            //Author                        ->  Request By
-            //Request_x0020_Status          ->  Request Status
-            //Request_x0020_Due_x0020_Date  -> Request Due Date
-
             CamlQuery camlQuery = new CamlQuery();
             ListItemCollection spListItems = spList.GetItems(camlQuery);
             clientContext.Load(spListItems, items => items.IncludeWithDefaultProperties(
-                item => item["NewColumn1"],
-                item => item["Request_x0020_Details"],
-                item => item["Author"],
-                item => item["Request_x0020_Status"],
-                item => item["Request_x0020_Due_x0020_Date"]));
+                item => item[sp_Request_ID],
+                item => item[sp_Request_Details],
+                item => item[sp_Request_By],
+                item => item[sp_Request_Status],
+                item => item[sp_Request_Due_Date]));
             clientContext.ExecuteQuery();
 
             ViewBag.List = spList;
@@ -77,9 +85,9 @@ namespace SPListTest1.Controllers
             List spList = clientContext.Web.Lists.GetByTitle(ListName);
             var info = new ListItemCreationInformation();
             var item = spList.AddItem(info);
-            item["Request_x0020_Details"] = details;
-            item["Author"] = userValue;
-            item["Request_x0020_Due_x0020_Date"] = due_date;
+            item[sp_Request_Details] = details;
+            item[sp_Request_By] = userValue;
+            item[sp_Request_Due_Date] = due_date;
             item.Update();
             clientContext.ExecuteQuery();
 
@@ -110,12 +118,12 @@ namespace SPListTest1.Controllers
             clientContext.Load(spListItem);
             clientContext.ExecuteQuery();
             
-            string Request_ID = (String)spListItem["NewColumn1"];
-            string Request_Details = (String)spListItem["Request_x0020_Details"];
-            string Request_Status = (String)spListItem["Request_x0020_Status"];
-            FieldUserValue Author = (FieldUserValue)spListItem["Author"];
+            string Request_ID = (String)spListItem[sp_Request_ID];
+            string Request_Details = (String)spListItem[sp_Request_Details];
+            string Request_Status = (String)spListItem[sp_Request_Status];
+            FieldUserValue Author = (FieldUserValue)spListItem[sp_Request_By];
             string Request_By = Author.LookupValue;
-            string Request_Due_Date = ((DateTime)spListItem["Request_x0020_Due_x0020_Date"]).ToShortDateString();
+            string Request_Due_Date = ((DateTime)spListItem[sp_Request_Due_Date]).ToShortDateString();
             
             ViewBag.Request_ID = Request_ID;
             ViewBag.Request_Details = Request_Details;
@@ -123,10 +131,6 @@ namespace SPListTest1.Controllers
             ViewBag.Request_By = Request_By;
             ViewBag.ID = ID;
             ViewBag.Request_Due_Date = Request_Due_Date;
-
-            //retrieving drop down menu items 
-            var choices_list = spListItem["Request_x0020_Status"].ToString();
-            ViewBag.Choices_List = choices_list;
 
             return View();
         }
@@ -138,9 +142,9 @@ namespace SPListTest1.Controllers
             List spList = clientContext.Web.Lists.GetByTitle(ListName);
             ListItem spListItem = spList.GetItemById(ID);
 
-            spListItem["Request_x0020_Details"] = details;
-            spListItem["Request_x0020_Status"] = status;
-            spListItem["Request_x0020_Due_x0020_Date"] = date;
+            spListItem[sp_Request_Details] = details;
+            spListItem[sp_Request_Status] = status;
+            spListItem[sp_Request_Due_Date] = date;
 
             spListItem.Update();
             clientContext.ExecuteQuery();
